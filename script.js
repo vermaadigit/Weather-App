5f54dd6c8e67c2bb34cb943e56738d4d6074a445
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-29 21:39:19 +0530

    Weather API Added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Weather App')
+const API_KEY = '454b589d9ab192483f018109056fa211';
+
+async function showWeather() {
+    // let latitude = 15.3333;
+    // let longitude = 75.6167;
+
+    let city = 'Goa';
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    const data = response.json();
+    console.log("Weather Data: ", data);
+}

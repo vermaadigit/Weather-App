31eb3c1b7122f7679180f1128cda3e30b8b1bfb8
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-30 19:30:32 +0530

    Tab Switcher Added

--- script.js
+++ script.js
@@ -1,66 +1,30 @@
-const API_KEY = '5310a0a3ba3c9e90578cbeb274588afa';
+const userTab = document.querySelector('[data-userWeather]');
+const searchTab = document.querySelector('[data-searchWeather]');
+const userContainer = document.querySelector('.weather-container');
+const grantAccessContainer = document.querySelector('.grantlocation-container');
+const searchForm = document.querySelector('[data-searchForm]');
+const loadingScreen = document.querySelector('.loading-container');
+const userInfoContainer = document.querySelector('.user-info-Container');
 
-async function fetchWeatherDetails() {
 
-    // let latitude = 15.3333;
-    // let longitude = 75.6167;
+//Initially needed Variables
 
-    try {
-        let city = 'Mumbai';
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-
-    const data = await response.json();
-    console.log("Weather Data: ", data);
-
-    // let temperature = data.main.temp; // Fetching the temperature from the API response
-    // console.log("Temperature: ", temperature);
-
-    // let humidity = data.main.humidity; // Fetching the humidity from the API response
-    // console.log("Humidity: ", humidity);
-
-    // let newPara = document.createElement('p');
-    // newPara.textContent = `City: ${data.name}, Temperature: ${temperature}°C, Humidity: ${humidity}%`;
-
-    // document.body.appendChild(newPara);
-    }
-    catch (error) {
-        console.log("Error: ", error);
-    }
-    
-}
-
-async function getCustomWeatherDetails() {
-    let latitude = 82.8628;
-    let longitude = 135.0000;
-
-    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    let data = await result.json();
-
-    console.log("Custom Weather Data: ", data);
-
-    let value = data.main.temp;
-    console.log("Temperature: ", value);
-}
+let currentTab = userTab;
+const API_KEY = "5310a0a3ba3c9e90578cbeb274588afa";
+currentTab.classList.add("current-tab"); 
 
 function switchTab(clickedTab) {
-    apiErrorContainer.classList.remove("active");
-
-    if (clickedTab != currentTab) {
+    if(clickedTab != currentTab) {
         currentTab.classList.remove('current-tab');
         currentTab = clickedTab;
         currentTab.classList.add('current-tab');
     }
-
-    if (!searchForm.classList.contains("active")) {
-        userInfoContainer.classList.remove("active");
-        grantAccessContainer.classList.remove("active");
-        searchForm.classList.add("active");
-    }
-    else
-    {
-        searchForm.classList.remove("active");
-        userInfoContainer.classList.remove("active");
-        getFromSessionStorage();
-    }
 }
+
+userTab.addEventListener('click', () => {
+    switchTab(userTab);
+});
+
+searchTab.addEventListener('click', () => {
+    switchTab(searchTab);
+});
